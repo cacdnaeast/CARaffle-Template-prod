37a14947905350d0e4356ad6e42e252c06076ef7
cacdnaeast <anand.chauhan@ca.com>
2017-06-18 11:55:26 -0400

    Update axa.php
    Updated to integrate Web App with CA App Experience Analytics (AXA) using the SDK snippet specifically for "MSF Demo - NA East'

--- plugins/axa.php
+++ plugins/axa.php
@@ -1,6 +1,6 @@
-<script type="text/javascript" id="ca_eum_ba" agent="browser" async
-        src="https://axa.saas.ca.com/mdo/v1/sdks/browser/BA.js"
-        data-profileUrl="https://collector.axa.saas.ca.com/api/1/urn:ca:tenantId:48B428C9-C483-B954-105A-A612C70885A2/urn:ca:appId:Raffle%20Application/profile?agent=browser"
-        data-tenantID="48B428C9-C483-B954-105A-A612C70885A2"
-        data-appID="Raffle Application"
-        data-appKey="fa732d70-269e-11e7-b5cd-994429ed59ce"></script>
+<script type="text/javascript" id="ca_eum_ba" agent=browser 
+        src="https://cloud.ca.com/mdo/v1/sdks/browser/BA.js" 
+        data-profileUrl="https://collector-axa.cloud.ca.com/api/1/urn:ca:tenantId:CACDNA01-USERSTORE/urn:ca:appId:Modern%20Software%20Factory%20Demo%20-%20NA%20East/profile?agent=browser" 
+        data-tenantID="CACDNA01-USERSTORE" data-appID="Modern Software Factory Demo - NA East" 
+        data-appKey="6233f540-543d-11e7-b4bd-09d934e182f7" >
+</script>
